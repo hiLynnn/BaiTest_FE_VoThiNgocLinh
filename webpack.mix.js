deb89e20decee614a3ea0dda556483cf9a622aa4
linh.vtngoc | apikey:8f2a4f33c6268b3a45ae6e4b557238bb91fe82edf71e397465d04c995cf264da <linh.vtngoc@fsofts.com>
2024-12-25 16:21:10 +0700

    save

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require('laravel-mix');
 
 
 mix.sass('asset/sass/common.scss', 'asset/css/common.css')
-    .js('asset/js/common.js', 'asset/js/compress-common.js');
+    // .js('asset/js/common.js', 'asset/js/compress-common.js');
 
 
